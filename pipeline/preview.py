b5879bd07c86cc9c4f6b6c851356560636a2bd4e
aaryavlal <aaryavlal12@gmail.com>
2026-06-08 12:29:54 -0700

    tweak6

--- pipeline/preview.py
+++ pipeline/preview.py
@@ -39,7 +39,7 @@ def camera_thread():
     cam.set_controls({
         "AwbEnable": False,
         # "ColourGains": (2.8, 0.3)
-         "ColourGains": (0.5, 3.5)
+         "ColourGains": (1.0, 3.25)
     })
 
     while True:
